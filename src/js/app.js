856c5381f59307a0eea85718c75a72e799740b5b
nochanghwan <nochanghwan@nochang.ubuntu>
2019-03-04 15:28:50 +0900

    app.js 메인함수추가

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
+    // pet 데이터를 로드히고 initWeb3()함수 호출
     $.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
@@ -24,17 +24,30 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    // 이더리움 블록체인과 상호작용
+    // 사용자계정을 검색하고 Transaction을 보내고 스마트컨트랙트와 상호작용
+
+    //이미 활성화된 web3 인스턴스가 있는지 체크
+    // web3Provider는 이더리움서버를 어떤걸로 할지 설정
+    if(typeof web3 !== 'undefined'){
+      App.web3Provider = web3.currentProvider;
+    }else{
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json',function(data){
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -43,10 +56,25 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
+
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance){
+      adoptionInstance = instance;
+
+      //call을 사용하면 트랜잭션보낼필요없이 데이터를읽을수 있음 -> 이더소비X
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters){
+      for(i=0; i< adopters.length; i++){
+        // 해당주소가있는 펫이 발견되면 입양버튼을 비활성화하고 버튼 텍스트를 Success로 변경
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err){
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -54,9 +82,25 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
@@ -64,5 +108,6 @@ App = {
 $(function() {
   $(window).load(function() {
     App.init();
+    // init함수실행 
   });
 });
--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -4,7 +4,8 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 //Assert는 테스트에서 통과 실패 반환 등 여부확인용
-//Deployed는 디플로이된 컨트랙트 주소가져옴
+//DeployedAddresses는 디플로이된 컨트랙트 주소가져옴
+//상위 두개의 import는 전역 Truffle 파일로 내부디렉토리에선X  
 
 contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
@@ -18,7 +19,7 @@ contract TestAdoption {
         Assert.equal(returnedId,expectedPetId, "결과1");
     }
 
-    // 입양한 펫 소유자 검색
+    // 입양한 펫의 소유자 검색
     function testGetAdopterAddressByPetId() public {
         address adopter = adoption.adopters(expectedPetId);
 
@@ -27,8 +28,9 @@ contract TestAdoption {
 
     //모든 애완동물 소유자 검색
     function testGetAdopterAddressByPetIdInArray() public {
+        //주어진 단일 키에 대한 단일 값만 반환하기 때문에 전체 배열을 위한 getter만듦
         address[16] memory adopters = adoption.getAdopters();
-
+        
         Assert.equal(adopters[expectedPetId], expectedAdopter, "결과3");
     }
 }
